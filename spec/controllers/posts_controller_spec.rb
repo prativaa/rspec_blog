dc7147d15e37e246b652fed478d0a779d222e838
Pratibha Bista <pratva.bsta@gmail.com>
2020-05-14 17:55:12 +0545

    created factories folder and added factory for post model created view file for show action added test for show action

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -10,9 +10,9 @@ RSpec.describe PostsController, type: :controller do
 
   describe "GET #show" do
     it "renders the show template" do
-      
-      # get :show, id: post.id
-      # expect(response).to render_template(:show)
+      post = FactoryBot.create(:post)
+      get :show, params: { id: post.id }
+      expect(response).to render_template(:show)
     end
   end
 end
