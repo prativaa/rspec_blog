1201284dfaba7d1048746f424a9f7af073970987
Pratibha Bista <pratva.bsta@gmail.com>
2020-05-14 18:36:16 +0545

    added test for new action for posts added instance variable to new action in posts_controller created new view file

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe PostsController, type: :controller do
   describe "GET #index" do
     it "renders the index template" do
       get :index
-      expect(response).to render_template(:index)
+      expect(response).to render_template :index
     end
   end
 
@@ -12,7 +12,14 @@ RSpec.describe PostsController, type: :controller do
     it "renders the show template" do
       post = FactoryBot.create(:post)
       get :show, params: { id: post.id }
-      expect(response).to render_template(:show)
+      expect(response).to render_template :show
+    end
+  end
+
+  describe "GET #new" do
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template :new
     end
   end
 end
